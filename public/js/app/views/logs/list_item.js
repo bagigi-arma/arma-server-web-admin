f29c1e8e63836c06cdb36acad5fa60cc7947e1dd
Fratee <elia.frate@gmail.com>
2023-07-10 11:38:34 +0200

    add players and fps stats on chart hover

--- public/js/app/views/logs/list_item.js
+++ public/js/app/views/logs/list_item.js
@@ -30,18 +30,17 @@ module.exports = Marionette.ItemView.extend({
       self.model.destroy()
     })
   },
-  
+
   viewPerf: function (event) {
 
-    function ExtractJson(text){
+    function ExtractJson(text) {
 
       let res = [];
       let pattern = /([0-9]+:[0-9]+:[0-9]+)\s?\"JsonStatus=(.*)\"/i;
       var lines = text.split('\n');
-      for(var i = 0;i < lines.length;i++){
+      for (var i = 0; i < lines.length; i++) {
         let result = lines[i].match(pattern);
-        if (result != null && result.length > 2)
-        {
+        if (result != null && result.length > 2) {
           let dataJson = result[2];
           const data = JSON.parse(dataJson.replaceAll("\"\"", "\""));
           data.time = result[1];
@@ -52,57 +51,91 @@ module.exports = Marionette.ItemView.extend({
       return res;
     };
 
-    function computePerf(pathToLog) {    
+    function computePerf(pathToLog) {
       var request = new XMLHttpRequest();
       request.open('GET', pathToLog, true);
       request.send(null);
       request.onreadystatechange = function () {
-          if (request.readyState === 4 && request.status === 200) {
-              const data = ExtractJson(request.responseText);
-              new chartjs.Chart(
-                  document.getElementById('chart'),
-                  {
-                      type: 'line',
-                      responsive: true,
-                      data: {
-                          labels: data.map(row => row.time),
-                          datasets: [
-                              {
-                                  label: 'FPS',
-                                  data: data.map(row => row.fps),
-                                  borderWidth : 1,
-                                  pointRadius: 0,
-                                  borderColor: [
-                                    'red'
-                                  ],
-                                  backgroundColor: [
-                                    'rgba(255, 0, 0, 0.2)'
-                                  ]
-                              },
-                              {
-                                  label: 'Players',
-                                  data: data.map(row => row.playerCount),
-                                  borderWidth : 1,
-                                  pointRadius: 0,
-                                  borderColor: [
-                                    'blue'
-                                  ],
-                                  backgroundColor: [
-                                    'rgba(0, 0, 255, 0.2)'
-                                  ]
-                              }
-                          ]
-                      }
-                  }
-              );
+        if (request.readyState === 4 && request.status === 200) {
+          const chartData = ExtractJson(request.responseText);
+
+
+          // compute fps statistics
+          var fps_arr = chartData.map(x => x.fps);
+
+          let fps_min = Number.POSITIVE_INFINITY;
+          let fps_avg = 0;
+          let fps_max = 0; 
+          
+          for (let i = 0; i < fps_arr.length; i++) {
+            fps_min = Math.min(fps_arr[i], fps_min);
+            fps_max = Math.max(fps_arr[i], fps_max);
+            fps_avg = fps_avg + parseFloat(fps_arr[i]);
           }
-      }  
+
+          if (fps_arr.length > 0)
+          {
+            fps_avg = fps_avg / fps_arr.length;       
+          }
+
+          document.getElementById('statsFPS').innerHTML = Math.round(fps_avg) + ", " + Math.round(fps_min) + ", " + Math.round(fps_max);
+
+          new chartjs.Chart(
+            document.getElementById('chart'),
+            {
+              type: 'line',
+              responsive: true,
+              data: {
+                labels: chartData.map(row => row.time),
+                datasets: [
+                  {
+                    label: 'FPS',
+                    data: chartData.map(row => row.fps),
+                    borderWidth: 1,
+                    pointRadius: 0,
+                    borderColor: [
+                      'red'
+                    ],
+                    backgroundColor: [
+                      'rgba(255, 0, 0, 0.2)'
+                    ]
+                  },
+                  {
+                    label: 'Players',
+                    data: chartData.map(row => row.playerCount),
+                    borderWidth: 1,
+                    pointRadius: 0,
+                    borderColor: [
+                      'blue'
+                    ],
+                    backgroundColor: [
+                      'rgba(0, 0, 255, 0.2)'
+                    ]
+                  }
+                ]
+              },
+              options: {
+                tooltips: {
+                  callbacks: {
+                    label: function(tooltipItem, data) {                      
+                      var label = data.datasets[tooltipItem.datasetIndex].label + ': ' + Math.round(tooltipItem.yLabel);
+                      var names = String(chartData[tooltipItem.index].players).replaceAll(',', ', ');
+                      document.getElementById("playersList").innerHTML = names;      
+                      return label;
+                    }
+                  }
+                }
+              }
+            }
+          );
+        }
+      }
     };
 
     var caller = event.target || event.srcElement;
     var logName = caller['target'];
     computePerf(logName);
     document.getElementById("chart-container").style.display = "block";
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: 'smooth' });    
   }
 })
